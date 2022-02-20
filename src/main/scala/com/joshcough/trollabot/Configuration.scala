94b561d708ee260ebfa828be0d0a36148161c58c
Josh Cough <joshcough@gmail.com>
2022-02-20 08:11:12 -0600

    test

--- src/main/scala/com/joshcough/trollabot/Configuration.scala
+++ src/main/scala/com/joshcough/trollabot/Configuration.scala
@@ -12,4 +12,5 @@ object Configuration {
   val ircServer: String = conf.getString("twitch.irc.server")
   val ircPort: Int = conf.getInt("twitch.irc.port")
   val ircCapabilities: Option[String] = Try(conf.getString("twitch.irc.capabilities")).toOption
+  val dbUrl: String = conf.getString("db_url")
 }
